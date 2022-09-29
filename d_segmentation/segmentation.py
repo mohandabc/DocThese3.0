dd0cf7537b7ec28b00016f0b955912430337b682
Mohand <mohand_abchiche@live.fr>
2022-09-29 10:29:54 -0700

    create function to get window given a center, and a function get windows in case of segmentation with superpixels

--- d_segmentation/segmentation.py
+++ d_segmentation/segmentation.py
@@ -1,9 +1,51 @@
 from skimage.transform import rescale, resize
 from keras.models import load_model
 from skimage import io
+from skimage.segmentation import mark_boundaries
 from tensorflow import convert_to_tensor
 import numpy as np
 from d_utils import utils, timer
+
+def get_window(img, centerX, centerY):
+    minX = centerX-15
+    maxX = centerX + 16
+    
+    minX2 = centerX-31
+    maxX2 = centerX + 32
+
+    minY = centerY - 15
+    maxY = centerY + 16
+    
+    minY2 = centerY - 31
+    maxY2 = centerY + 32
+
+    win1 = img[minX:maxX, minY:maxY]
+    win2 = img[minX2:maxX2, minY2:maxY2]
+    return win1,win2
+
+def generate_windows_superpixels(img, sp_map):
+    superpixels = np.sort(np.unique(sp_map))
+    batch1 = []
+    batch2 = []
+    for c in superpixels:
+        mask = sp_map==c
+        x1, x2, y1, y2 = utils.find_borders(mask)
+        center_x = (x1 + x2)//2 + sp_map.shape[0]
+        center_y = (y1 + y2)//2 + sp_map.shape[1]
+        win1,win2 = get_window(img, center_x, center_y)
+        batch1.append(win1)
+        batch2.append(win2)
+        if len(batch1) == 64:
+                batch1_t = convert_to_tensor(batch1)
+                batch2_t = convert_to_tensor(batch2)
+                batch1 = []
+                batch2 = []
+                yield [batch1_t, batch2_t], []
+    if len(batch1)>0:
+        batch1_t = convert_to_tensor(batch1)
+        batch2_t = convert_to_tensor(batch2)
+        yield [batch1_t, batch2_t], [] #Last batch may be smaller than 64
+
 
 def genrate_windows(img, w, l):
     """Generate 31x31 and 63x63 windows for each pixel in img"""
@@ -12,20 +54,7 @@ def genrate_windows(img, w, l):
     for line in range(w, 2*w):
         for col in range(l, 2*l):
 
-            minX = line-15
-            maxX = line + 16
-            
-            minX2 = line-31
-            maxX2 = line + 32
-
-            minY = col - 15
-            maxY = col + 16
-            
-            minY2 = col - 31
-            maxY2 = col + 32
-
-            win1 = img[minX:maxX, minY:maxY]
-            win2 = img[minX2:maxX2, minY2:maxY2]
+            win1, win2 = get_window(img, line, col)
 
             batch1.append(win1)
             batch2.append(win2)
