73b77e5c59b50b189e9419af733ff8c3a4762068
Mohand <mohand_abchiche@live.fr>
2022-09-30 07:39:20 -0700

    don't ignore stemp file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='d_packages',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='Mohand',
+author_email='author@email.com',
+license='MIT',
+packages =find_packages(),
+zip_safe=False)
